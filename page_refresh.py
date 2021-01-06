43c1550592df215adcd4e2ac266fc7fc0aec550d
Neuron_Surge <63504515+NeuronSurge@users.noreply.github.com>
2021-01-06 09:16:32 -0500

    Update page_refresh.py

--- page_refresh.py
+++ page_refresh.py
@@ -4,17 +4,17 @@ import webbrowser
 #from selenium import webdriver
 from time import sleep
 
-url = input('What URL would you like to reload?')
+url = input('What URL would you like to reload?') #insert reload incl http://
 
 refresh = 0
 
-while refresh <= 10:
+while True:
 	print(refresh)
 	webbrowser.open(url, new=0)
 	sleep(2)
-else:
-	print('Refreshed 9999 times')
-refresh += 1
-
+	refresh += 1
+	
+	if refresh == #insert number or reloads
+	break
 
 
