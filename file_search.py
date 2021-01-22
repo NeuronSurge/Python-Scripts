5f0616eb521256db3d351e184c7ebd87b25e694a
Neuron_Surge <63504515+NeuronSurge@users.noreply.github.com>
2021-01-22 08:30:03 -0500

    Update file_search.py

--- file_search.py
+++ file_search.py
@@ -5,7 +5,7 @@ import fnmatch
 
 for root, dirs, files in os.walk('test/'): # change path
    for file in files:
-       with open(os.path.join(root, file), "r") as auto:
-            f = auto.read(1000)
-            if "kirsty" in f: # change string to search for
+       with open(os.path.join(root, file), "r") as findit:
+            f = findit.read(1000)
+            if "" in f: # change string to search for
                   print("String found in", file)
